b55ce56d612b201d249a6b932f9a7c958812d442
Sara T <sahara.tijol@gmail.com>
2020-10-16 13:53:46 -0500

    Add: Contact object. Getter and setters setup

---
+++ src/Contact.java
@@ -0,0 +1,30 @@
+
+
+public class Contact {
+    private String name;
+    private long number;
+
+    // Constructor
+    public Contact (String name, long number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public long getNumber() {
+        return this.number;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+}
